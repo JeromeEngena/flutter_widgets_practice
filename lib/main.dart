9c94be2ef97fd5ef4c4909a1758e197837f562cc
Jerome <jembrian7@gmail.com>
2023-12-09 14:59:17 +0300

    added image assets

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.green),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blue,
@@ -27,21 +27,34 @@ class _MyAppState extends State<MyApp> {
           ),
           centerTitle: true,
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {});
-                },
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                child: const Text("Click Here",
-                    style: TextStyle(color: Colors.white)),
-              ),
-            ),
-          ],
+
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset("images/sky.jpg"),
+              Image.network(
+                  "https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg.webp"),
+              Image.asset("images/sky.jpg"),
+            ],
+          ),
         ),
+        //
+        //
+        // Column(
+        //   children: [
+        //     Padding(
+        //       padding: const EdgeInsets.all(8.0),
+        //       child: ElevatedButton(
+        //         onPressed: () {
+
+        //         },
+        //         style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+        //         child: const Text("Click Here",
+        //             style: TextStyle(color: Colors.white)),
+        //       ),
+        //     ),
+        //   ],
+        // ),
         bottomNavigationBar: BottomAppBar(
           color: Colors.blue,
           child: IconButton(
